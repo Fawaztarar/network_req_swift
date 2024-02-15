578b593b6364f858fd04fb818e3bd4280e997ac9
Fawaztarar <fawaztarar@gmail.com>
2024-02-15 11:02:06 +0000

    Initial Commit

---
+++ NetworkingApp/NetworkingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingAppApp.swift
+//  NetworkingApp
+//
+//  Created by Fawaz Tarar on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
